3da0455b0a757f2e5e4c164ee0a0665cde162e69
Caitlin Lamprecht <42557454+MagicWishbone6@users.noreply.github.com>
2019-04-24 19:59:53 -0500

    Commit my changed docs

--- wheel_of_fortune.js
+++ wheel_of_fortune.js
@@ -8,10 +8,15 @@
 
 // Set a secret word!
 
-var word = 'ANSWER'; // ALL CAPS
+var word = 'GOOSE'; // ALL CAPS
+console.log(word.length); 
+
 
 // Ask player if they would like to guess a letter or guess the
 // solution.
+console.log("Hello Contestant, it is your turn!")
+console.log("Would you like to guess a letter, or would you like to guess the solution?")
+console.log("Say 'LETTER' or 'ANSWER'.")
 
 var isGuessingLetter = true; // set to false to guess solution
 
@@ -19,28 +24,46 @@ var isGuessingLetter = true; // set to false to guess solution
 // guessing. If they are guessing the word, then set the word
 // as their guess.
 
-var guess = 'A'; // UPPERCASE
+var guess = 'G'; // UPPERCASE
 
 // SCRIPT ////////////////////////////////////////////////////////////
+var guessedWord = "-----"
 
+console.log(guessedWord);
 // If guessing a letter...
-
-	// If the word contains the letter...
-
-		// Print back the word with all instances of that letter revealed.
-
+if (isGuessingLetter = true);
+  while (guessedWord.includes("-") = true);
+  // If the word contains the letter...
+	  if (word.includes(guess) = true);
+  // Print back the word with all instances of that letter revealed
+      var n = 0;
+      while (n <= word.length);
+        if (word[n]=guess);
+          guessedWord[n]=guess;
+          n += 1;
+        else;
+          n += 1;
+		  console.log(guessedWord);
 	// If the word does not contain the letter...
-
-		// Print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
-
+	  if (word.includes(guess) = false);
+	// Print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
+		  console.log("Sorry, no " + guess + "!");
 // If guessing the word...
-
-	// If they are correct...
-
-		// Print "Congratulations! You won Wheel Of Fortune!"
-
+if (isGuessingLetter = false);
+  var n = 0;
+  while (n <= word.length);
+    if (guess[n] = word[n]);
+      n+=1;
+	else
+		console.log("Nope, keep trying!");
 	// If they are incorrect...
-
-		// Print back "Nope, keep trying!"
+	// Print back "Nope, keep trying!"
+    	n = (word.length + 2);
+  n = 0;
+  if (n = word.length);  
+	// If they are correct...
+  // Print "Congratulations! You won Wheel Of Fortune!"
+		console.log("Congratulations! You won Wheel of Fortune!");
+  n= (word.length + 5);
 
 // End of script!
